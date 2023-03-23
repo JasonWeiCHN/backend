93d04ae887a45ff762dfac6fbddb05f8794bdca0
JasonWei <89176380+JasonWei2021@users.noreply.github.com>
2023-03-23 15:15:26 +0800

    feat(front): 搭建后台管理系统主框架 实现用户新增

--- frontend/wei-front/src/app/app.routes.ts
+++ frontend/wei-front/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import {MainComponent} from "./pages/main/main.component";
+import {UserComponent} from "./pages/user/user.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            {
+                path: 'user',
+                component: UserComponent,
+            }
+        ]
+    },
+];
---
+++ frontend/wei-front/src/app/pages/main/shared/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wei-front-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  public title = 'wei-front';
+  public username = 'username';
+
+}
