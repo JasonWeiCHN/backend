f164f494dfbc0306a68c368feb641bd8f240aea7
JasonWei <89176380+JasonWei2021@users.noreply.github.com>
2023-04-12 14:50:40 +0800

    feat: angular 学习研究使用

---
+++ frontend/a-front/apps/learning/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "a-front-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "learning";
+}
