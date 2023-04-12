edfec5dae47666ec004fb49fa68849ccd7c271bb
JasonWei <89176380+JasonWei2021@users.noreply.github.com>
2023-04-12 16:25:56 +0800

    feat: add test

--- frontend/a-front/apps/learning/src/app/app.component.ts
+++ frontend/a-front/apps/learning/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import {FormGroup} from "@angular/forms";
+import {FormlyFieldConfig} from "@ngx-formly/core";
 
 @Component({
   selector: "a-front-root",
@@ -7,4 +9,31 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "learning";
+
+  form = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    },
+    {
+      key: 'name',
+      type: 'input',
+      props: {
+        label: 'Your name',
+        placeholder: 'Enter name',
+        required: true,
+      }
+    }
+  ];
+
+  onSubmit(model: any) {
+    console.log(model);
+  }
 }
--- frontend/a-front/apps/learning/src/app/app.module.ts
+++ frontend/a-front/apps/learning/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { appRoutes } from "./app.routes";
 import { NxWelcomeComponent } from "./nx-welcome.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent],
