5f7ede789f94f02777a938ea9e2363d756a99c8a
JasonWei <89176380+JasonWei2021@users.noreply.github.com>
2023-04-14 14:30:16 +0800

    feat(static): 项目分文件

---
+++ frontend/static/c/config.js
@@ -0,0 +1,11 @@
+// [项目配置项]
+// 初始值 - y轴
+const initPrice = 300;
+// 变化率
+const initChangeRate = 0;
+// x 轴的向右偏移量
+const xOffset = 50;
+// 平坡或者上升时的颜色
+const upColor = "red";
+// 下降时的颜色
+const downColor = "blue";
