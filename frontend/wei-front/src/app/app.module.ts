f0fcfc3d0632889bbb7b61ca74721fd6a0b4b882
JasonWei <89176380+JasonWei2021@users.noreply.github.com>
2023-05-22 15:22:44 +0800

    feat: article with ngx-editor

--- frontend/wei-front/src/app/app.module.ts
+++ frontend/wei-front/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { AddUserComponent } from './pages/user/pages/add-user/add-user.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
@@ -29,6 +29,7 @@ import { TaskModule } from './pages/task/task.module';
 import { HotTableModule } from '@handsontable/angular';
 import { ProductComponent } from './pages/product/product.component';
 import { ArticleComponent } from './pages/article/article.component';
+import { NgxEditorModule } from 'ngx-editor';
 
 @NgModule({
   declarations: [
@@ -62,6 +63,9 @@ import { ArticleComponent } from './pages/article/article.component';
     MatNativeDateModule,
     MainModule,
     TaskModule,
+    NgxEditorModule,
+    NgxEditorModule,
+    FormsModule,
   ],
   providers: [
     {
--- frontend/wei-front/src/app/pages/article/article.component.ts
+++ frontend/wei-front/src/app/pages/article/article.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Editor, Toolbar, Validators } from 'ngx-editor';
+import jsonDoc from './doc';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'wei-front-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent {}
+export class ArticleComponent implements OnInit, OnDestroy {
+  // editordoc = jsonDoc;
+
+  public editor!: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
+  form = new FormGroup({
+    editorContent: new FormControl({ value: jsonDoc, disabled: false }, Validators.required()),
+  });
+
+  // get doc(): AbstractControl {
+  //   return this.form.get('editorContent');
+  // }
+
+  ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+}
