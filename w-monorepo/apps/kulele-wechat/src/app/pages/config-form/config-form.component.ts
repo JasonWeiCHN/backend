e0ab30ed0832975102e986d27270debfcec1b0ca
weigy <422350285@qq.com>
2025-08-04 15:35:26 +0800

    20250804-2

---
+++ w-monorepo/apps/kulele-wechat/src/app/pages/config-form/config-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-config-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './config-form.component.html',
+  styleUrl: './config-form.component.scss',
+})
+export class ConfigFormComponent {}
---
+++ w-monorepo/apps/kulele-wechat/src/app/pages/games-form/games-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-games-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './games-form.component.html',
+  styleUrl: './games-form.component.scss',
+})
+export class GamesFormComponent {}
